5b664fccef9218c7122a9cc00d89712351eae625
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-11-19 19:11:45 +0530

    Image and Video upload/display functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-box';
+  title = "angular-box";
+  filePath : any = '';
+  onUpload(event: any){
+    const files = event.target.files;
+    console.log("files", files[0]);
+    
+    console.log("working!!", this.filePath)
+    var reader = new FileReader();
+    reader.addEventListener("load", ()=>{
+      console.log("Reader Loaded", reader.result);
+      this.filePath = reader.result;
+    })
+    reader.readAsDataURL(files[0]);
+  }
 }
